331d96ef91e2e268e8275c4f5cc96e4293206eec
Jaswant Rajpurohit <rajpurohitjaswant587@gmail.com>
2025-07-16 10:54:17 +0000

    Datatypes Basics

---
+++ 01_basics/03_dataype.js
@@ -0,0 +1,12 @@
+//Primitive DataTypes
+Number = 39 //normal number digit 
+String ="Jaswant" // String is a collection of character
+Boolean ="true" // it can be either true or false
+null 
+undefined => //variable is declare but not defined
+Symbol => //Special Character
+BigInt => // when number data is two wide
+
+//Non Primitive Datatypes
+
+Object  //object is a key pair values datatypes which hold data
