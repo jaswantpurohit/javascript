425f03b06973162aa0ae4700c6ff1764d8952c80
Jaswant Rajpurohit <rajpurohitjaswant587@gmail.com>
2025-07-16 10:20:44 +0000

    Variable Basic

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,13 @@
+const accountId = 454545;
+let accountEmail = "rajpurohitjaswant587@gmail.com";
+let accountPassword = "3434234";
+accountCity ="Ahmedabad"
+
+let accountState;
+
+// accountId = 33423;
+accountEmail = "jackpeter@gmail.com";
+accountPassword ="3434643223423";
+accountCity ="Udaipur"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
