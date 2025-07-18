fe977160ae2c775576012a4253edfcbdb3def621
Jaswant Rajpurohit <rajpurohitjaswant587@gmail.com>
2025-07-18 10:58:21 +0000

    Logical and and or operator code

---
+++ 01_basics/04_boolean.js
@@ -0,0 +1,33 @@
+// let age = 15;
+// if( age > 18){
+//     console.log(age);
+// }else{
+//     console.log("You are not eligable");
+// }
+
+// let age = 70;
+
+// if( age > 65 ){
+//     console.log("You can not drive as your age is not match");
+// } else if ( age > 18 ){
+//     console.log("You can drive");
+// } else{
+//     console.log("You can us Bycycle")
+// }
+
+// && => both condition should be true
+// || => from both only one condition should be true
+const age = 12;
+console.log( age < 18 );
+console.log( age > 65 );
+if( age < 18 && age >65 ){
+    console.log(" you can drive ")
+}
+
+const ageOne = 14;
+console.log( ageOne < 18 );
+console.log( ageOne > 65 );
+if( ageOne < 18 || ageOne >65 ){
+    console.log(" you can drive ")
+}
+
