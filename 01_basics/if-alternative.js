57601115f2916bd549170384d197be360386183f
Jaswant Rajpurohit <rajpurohitjaswant587@gmail.com>
2025-07-22 17:57:28 +0000

    If statement changes

---
+++ 01_basics/if-alternative.js
@@ -0,0 +1,23 @@
+//Ternary Operator
+
+// let age = 12;
+// let result = age > 18 ? "Adult" : "Kid";
+// console.log(result);
+
+//Guard Operator
+
+// let age = 32;
+// let finalAge = age || 18;
+// console.log(age);
+// console.log(finalAge);
+
+
+//Default Operator
+
+let age = 18;
+let finalAge = age ?? 18;
+console.log(age);
+console.log(finalAge);
+
+
+
